47726e20f02c884e133ac1c78014c8d5e59b6b50
sbal13 <sbal13@gmail.com>
2018-10-18 12:15:51 -0400

    deliverables

--- bin/run.rb
+++ bin/run.rb
@@ -2,7 +2,7 @@ require_relative "../config/environment.rb"
 
 
 a1 = Alien.new("Klingon", "Spanish")
-a2 = Alien.new("Kryptonian", "Kryptic")
+a2 = Alien.new("Kryptonian", "Praetorian")
 a3 = Alien.new("Predator", "Praetorian")
 a4 = Alien.new("Autobots", "English")
 
@@ -10,7 +10,7 @@ a4 = Alien.new("Autobots", "English")
 p1 = Planet.new("New Klingon", "cold")
 p2 = Planet.new("Krypton", "temperate")
 p3 = Planet.new("Columbia", "hot")
-p4 = Planet.new("Cybertron", "frozen")
+p4 = Planet.new("Cybertron", "cold")
 
 
 pop1 = Population.new(a1, p1, 1000)
@@ -22,3 +22,13 @@ pop6 = Population.new(a2, p4, 1000)
 pop7 = Population.new(a4, p3, 1000)
 
 Pry.start
+
+
+Write methods that:
+
+# Return the Alien with the most numerous populations
+# Return the Alien that lives on the most planets
+# Return the Planet with the most alien races
+# Given a language returns an arry of aliens that speak language
+# Given a climate returns an array of all planets with that climate
+
